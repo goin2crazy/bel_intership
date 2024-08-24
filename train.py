c48d8c672bf2dbe2b80148bf0f520a36d95a5af3
Double Cringe <92397500+goin2crazy@users.noreply.github.com>
2024-08-24 21:30:55 +0500

    Update train.py

--- train.py
+++ train.py
@@ -1,55 +1,105 @@
+import pickle
+from picker_model import TargetModel
 from dataprocessor import load_data
-from tensorflow.data import Dataset 
+
 import tensorflow as tf
+from tensorflow.data import Dataset
+from tqdm.notebook import tqdm
 
-tf.executing_eagerly()
+from tensorflow.keras.optimizers import AdamW
+from tensorflow.keras.callbacks import EarlyStopping
+import keras 
+
+def prepare_data(data):
+  image_links = list()
+  labels = list()
+
+  for item in data:
+    image_links.extend([k for k, v in item.items()])
+    labels.extend([v for k, v in item.items()])
+
+  return image_links, labels
 
 
-from picker_model import TargetModel 
+def get_data(file_path='/content/correct_answers (3).pkl'): 
+  with open(file_path, 'rb') as f:
+    data1 = pickle.load(f)
+  
+  image_links, labels = prepare_data(data1)
+  return image_links, labels
 
-def image_mapping_fn(image_link): 
-  image_link = bytes.decode(image_link.numpy())
-  # image_link = bytes.decode(image_link)
+def create_dataset(image_links, labels, batch_size=8):
+  images = [load_data(l) for l in tqdm(image_links)]
 
-  return load_data(image_link)
+  dataset = Dataset.from_tensor_slices((images,labels))
 
-class Trainer(TargetModel): 
-  def __init__(self, 
-               dataset = None, 
-               dataset_path=None): 
-    super().__init__()
+  dataset = dataset.shuffle(64)
+  dataset = dataset.prefetch(64)
+
+  return dataset.batch(batch_size)
+
+def train_model(model, ds, num_epoch = 100): 
+  early_stopping = EarlyStopping(monitor='accuracy',
+                                 patience=25,
+                                 mode='max',
+                                 restore_best_weights=True
+                                 )
+  model.compile(optimizer=AdamW(learning_rate=1e-6, weight_decay=0.001),
+                # binary crossentropy
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+  model.fit(ds, epochs=num_epoch)
+  return model
+
+def save_model(model, save_path="checkpoint.weights.h5"): 
+  try: 
+    model.save_weights(save_path, overwrite=True)
+    print("succesfully saved :>")
+  except Exception as e: 
+    print("Caugh error while saving:", e)
+  
+
+def run(data_path, 
+        batch_size=8, 
+        chunk_size=300, 
+        num_epoch=100, 
+        save=True, 
+       ): 
+  img_links, labels = get_data(data_path)
+
+  model = TargetModel().model
+  
+  for i in range(len(img_links) // chunk_size): 
+    chunk_start = i * chunk_size
+    chunk_end = i * chunk_size + chunk_size
     
-    if dataset == None: 
-      dataset = self.read_from_dataset_path(dataset_path)
-    self.dataset = dataset
+    ds = create_dataset(image_links[chunk_start:chunk_end], 
+                        labels[chunk_start:chunk_end], 
+                        batch_size = batch_size)
+    model = train_model(model, ds, num_epoch)
 
-    self.dataset_dict = {} 
-    for item in self.dataset: 
-      self.dataset_dict = {**self.dataset_dict, **item}
+    if save: 
+      save_model(model)
+  return model
 
-  def read_from_dataset_path(self, dataset_folder): 
-    json_files = []
-    for root, dirs, files in os.walk(dataset_folder):
-      for file in files:
-        if file.endswith('.json'):
-          json_files.append(os.path.join(root, file))
+if __name__ == "__main__": if __name__ == "__main__":
+    import argparse
 
-    data = []
-    for json_file in json_files:
-      with open(json_file, 'r') as f:
-        data.append(json.load(f))
-    return data 
+    parser = argparse.ArgumentParser(description="Train the model with specified parameters.")
 
-  def build_dataset(self): 
-    image_links = list(self.dataset_dict.keys())
-    labels = list(self.dataset_dict.values()) 
-    
-    self.dataset = (Dataset.from_tensor_slices({"image": image_links, "l": labels})
-    .map(lambda i: {"image": tf.py_function(image_mapping_fn, [(i['image'])], [tf.float32]), "label": i['l']}))
-    return self.dataset.batch(128)
+    parser.add_argument('--data-path', type=str, help="Path to the dataset file")
+    parser.add_argument('--batch-size', type=int, default=128, help="Batch size for training")
+    parser.add_argument('--chunk-size', type=int, default=300, help="Chunk size for dividing the dataset")
+    parser.add_argument('--num-epoch', type=int, default=100, help="Number of epochs to train the model")
+    parser.add_argument('--save', action='store_true', help="Save the model after training")
 
-  def train(self): 
-    dataset = self.build_dataset()
+    args = parser.parse_args()
 
-trainer = Trainer(dataset = correct_answers)
-trainer.train()
+    model = run(data_path=args.data_path,
+                batch_size=args.batch_size,
+                chunk_size=args.chunk_size,
+                num_epoch=args.num_epoch,
+                save=args.save)
+
+# Example run
+# python script_name.py /path/to/data.csv --batch_size 16 --chunk_size 500 --num_epoch 50 --save
